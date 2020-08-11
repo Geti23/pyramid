99f3ec5a1acb7052e4d4169a6a44cbf84262ad2c
Getoar Krasniqi <45102059+Geti23@users.noreply.github.com>
2020-08-11 20:31:03 +0200

    Create Colors.java

---
+++ Colors.java
@@ -0,0 +1,19 @@
+import java.awt.Color;
+import javax.vecmath.Color3f;
+
+class Colors {
+	static final Color3f RED = new Color3f(new Color(0xFF0000));
+	static final Color3f PINK = new Color3f(new Color(0xFF3E96));
+	static final Color3f ORANGE = new Color3f(new Color(0xFF8800));
+	static final Color3f YELLOW = new Color3f(new Color(0xFFFF00));
+	static final Color3f GREEN = new Color3f(new Color(0x00FF00));
+	static final Color3f BLUE = new Color3f(new Color(0x0000FF));
+	static final Color3f CYAN = new Color3f(new Color(0x00FFFF));
+	static final Color3f PURPLE = new Color3f(new Color(0xFF00FF));
+	static final Color3f BLACK = new Color3f(new Color(0x000000));
+	static final Color3f WHITE = new Color3f(new Color(0xFFFFFF));
+	static final Color3f GRAY = new Color3f(new Color(0x888888));
+	static final Color3f DARK_GRAY = new Color3f(new Color(0xFF303030));
+  static final Color3f DARK_RED = new Color3f(new Color(0x880000));
+}
+
